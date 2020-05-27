d1b8e8af1c4bf3e62e49a34f9a7bda5b55dac9e6
Jarrod <jtomasek@stedwards.edu>
2020-05-27 15:14:58 -0500

    Added BodyParser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
 const app: Application = express();
+
+// body parser extracts the request body & exposes it on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello")
