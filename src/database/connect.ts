48f944525437c218f38264110817227bff10088d
Jarrod <jtomasek@stedwards.edu>
2020-05-27 16:15:26 -0500

    Added MySQL Database Connection

---
+++ src/database/connect.ts
@@ -0,0 +1,25 @@
+import mysql, {Connection, MysqlError} from 'mysql';
+
+export class DBConnection {
+
+    myConnection: Connection;
+
+    constructor() {
+        this.myConnection = mysql.createConnection("mysql://bd4dfbdb1ee11c:82ab9c56@us-cdbr-east-05.cleardb.net/heroku_c0fb01ad990e85c?reconnect=true");
+    }
+
+    connect() {
+        this.myConnection.connect((err: MysqlError) => {
+            if (err) {
+                console.error("Error Connecting: " + err.stack);
+                return;
+            }
+        
+            console.log("Database Connected");
+        });
+    }
+
+    disconnect() {
+
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
+// Database
+import { DBConnection } from './database/connect'
+
 const app: Application = express();
 
 // body parser extracts the request body & exposes it on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello")
-});
 
+// Create Connection to Database
+const dbConnection = new DBConnection().connect();
 
 app.listen((process.env.PORT || 5000), () => {
     console.log("Server Running")
-})
+});
+
+// Default Endpoint 
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello")
+});
