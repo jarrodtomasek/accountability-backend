1fe5201f5abe20e371a85ae0e3500fb04a4db1a9
Jarrod <jtomasek@stedwards.edu>
2020-05-27 17:19:55 -0500

    Table Fix

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -37,18 +37,18 @@ export class DBConnection {
     createTaskTable() {
         var sql = `CREATE TABLE Task (user_id INT AUTO_INCREMENT PRIMARY KEY,email VARCHAR(75),start_date DATE,due_date DATE,status TINYINT NOT NULL,priority TINYINT NOT NULL,description TEXT,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)  ENGINE=INNODB;`
         var sql2 = 'DROP TABLE Task'
-        this.myConnection.query(sql2, (err, result) => {
+        this.myConnection.query(sql, (err, result) => {
             if (err) throw err;
-            console.log("Task Table Deleted")
+            console.log("Task Table Created")
         })
     }
 
     createGroupTable() {
         var sql = `CREATE TABLE Group (group_id INT AUTO_INCREMENT PRIMARY KEY,group_name VARCHAR(150),number_of_members INT DEFAULT 0,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)  ENGINE=INNODB;`
         var sql2 = "DROP TABLE Group"
-        this.myConnection.query(sql2, (err, result) => {
+        this.myConnection.query(sql, (err, result) => {
             if (err) throw err;
-            console.log("Group Table Deleted")
+            console.log("Group Table Created")
         })
     }
 }
