e32e783015121005a5492c56f13d95daef6f36dd
Jarrod <jtomasek@stedwards.edu>
2020-05-29 16:14:24 -0500

    Added options for preflight requests. Added headers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 
 // Database
@@ -8,10 +8,23 @@ import { DBConnection } from './database/connect'
 import UserAuth from './routes/UserAuth'
 
 const app: Application = express();
+const CLIENT_PORT = 8100;
 
 // body parser extracts the request body & exposes it on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + CLIENT_PORT);
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+	next();
+});
+
+// pre-flight requests
+app.options('*', (req: Request, res: Response) => {
+  res.send(200)
+})
 
 // Create Connection to Database
 const mySQLConnection = new DBConnection();
