f30f7c0f35a2a1740911502b54696ed6d615786a
Jarrod <jtomasek@stedwards.edu>
2020-07-18 16:56:15 -0500

    added more logs

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -11,7 +11,8 @@ export class DBConnection {
 
     constructor() {
         console.log(process.env.TEST_URL)
-        this.myConnection = mysql.createConnection((process.env.CLEARDB_DATABASE_URL).toString());
+        console.log(process.env.CLEARDB_DATABASE_URL)
+        this.myConnection = mysql.createConnection("mysql://bd4dfbdb1ee11c:82ab9c56@us-cdbr-east-05.cleardb.net/heroku_c0fb01ad990e85c?reconnect=true");
     }
 
     connect() {
